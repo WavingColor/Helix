3e271e40f7ae5d3b55b1adad5860de898c54a53f
WavingColor <color_free@foxmail.com>
2016-03-19 10:56:44 +0800

    Initial Commit

---
+++ Helix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Helix
+//
+//  Created by mac on 16/3/19.
+//  Copyright © 2016年 高亮军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Helix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Helix
+//
+//  Created by mac on 16/3/19.
+//  Copyright © 2016年 高亮军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
